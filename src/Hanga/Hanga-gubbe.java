3e77140d03dc4b565bbf3b57cf734fc26973cc0d
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-01-15 11:13:31 +0100

    15/1-2021
    Har inte gått så jätte bra idag och kommer behöva hjälp nästa lektion.

---
+++ src/Hanga/Hanga-gubbe.java
@@ -0,0 +1,12 @@
+package Hanga;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class gubbe {
+    public static void main(String []args) {
+        Random rand = new Random();
+         int rand_int2 = rand.nextInt(10);
+          System.out.println("Random Integers: "+rand_int2);
+    }
+}
