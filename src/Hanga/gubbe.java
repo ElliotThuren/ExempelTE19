3725b7251f09782001ff6e71323f3af849f05301
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-02-02 10:57:50 +0100

    2/2-2021

--- src/Hanga/gubbe.java
+++ src/Hanga/gubbe.java
@@ -2,6 +2,7 @@ package Hanga;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class gubbe {
     public static void main(String []args) {
@@ -17,12 +18,43 @@ public class gubbe {
 
           System.out.println(
                   "Valt ord: " + ord[rr]);
-
+          int l = ord[rr].length();
           String guess = "";
+          for (int i = 0 ; i < l ; i++) {
+              guess = guess+"_";
+          }
 
-          while(!guess.equals()){
+          Scanner keyboardInput = new Scanner(System.in);
+
+          while(!guess.equals(ord[rr])){
+              System.out.println(guess);
               System.out.print("Gissa en bokstav: ");
-              guess = keyboardInput.nextLine();
+              char guessedChar = keyboardInput.nextLine().charAt(0);
+            if (ord[rr].contains(""+guessedChar)) {
+                StringBuilder sb = new StringBuilder(guess);
+                for(int i = 0; i<guess.length(); i ++) {
+                        if (ord[rr].charAt(i) == guessedChar) {
+                            sb.setCharAt(i, guessedChar);
+                        }
+
+                }
+                guess = sb.toString();
+                public static final int NUM_GUESSES = 10;
+                private char[] guessed;
+                this.guessed = new char[NUM_GUESSES];
+                System.out.print("You so far guessed: ");
+                for (int i = 0; i < guessed.length; ++i) {
+                    if (i > 0) {
+                        System.out.print(", ");
+                    }
+                    if (guessed[i] != 0) {
+                        System.out.print(guessed[i]);
+                    } else {
+                        break; // stop the loop as soon as we run into a 0
+                    }
+                }
+                System.out.println(".");
+            }
           }
 
     }
